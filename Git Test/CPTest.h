b32437ddcd7e388222fc9c17b9f50384e0b93bd6
iHac71 <ihac71@gmail.com>
2014-02-22 16:35:58 -0600

    added a test class

---
+++ Git Test/CPTest.h
@@ -0,0 +1,13 @@
+//
+//  CPTest.h
+//  Git Test
+//
+//  Created by Chuck on 2/22/14.
+//  Copyright (c) 2014 iHac71. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CPTest : NSObject
+
+@end
---
+++ Git Test/CPTest.m
@@ -0,0 +1,13 @@
+//
+//  CPTest.m
+//  Git Test
+//
+//  Created by Chuck on 2/22/14.
+//  Copyright (c) 2014 iHac71. All rights reserved.
+//
+
+#import "CPTest.h"
+
+@implementation CPTest
+
+@end
