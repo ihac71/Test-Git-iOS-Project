7fc55ff149c21afdfa014d828267154f7faee2f3
Chuck <tfdresq@Charles-Pottss-iMac-2.local>
2014-02-22 16:06:04 -0600

    Initial Commit

---
+++ Git Test/CPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CPAppDelegate.h
+//  Git Test
+//
+//  Created by Chuck on 2/22/14.
+//  Copyright (c) 2014 iHac71. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/CPViewController.h
@@ -0,0 +1,13 @@
+//
+//  CPViewController.h
+//  Git Test
+//
+//  Created by Chuck on 2/22/14.
+//  Copyright (c) 2014 iHac71. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPViewController : UIViewController
+
+@end
